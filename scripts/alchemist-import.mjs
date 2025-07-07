fc1eb5f898ac08489699096b9e1e889ed57b904d
Thanacus <thanacus@gmail.com>
2025-07-07 18:51:44 -0400

    Fixing issue with button

--- scripts/alchemist-import.mjs
+++ scripts/alchemist-import.mjs
@@ -1,12 +1,12 @@
 import { presentDialog } from './import-application.mjs'
 
 Hooks.once('ready', function() {   
-    Hooks.on('renderSceneDirectory', (html, entryOptions) => {
+    Hooks.on('activateSceneDirectory', (html, entryOptions) => {
         const button = document.createElement("button")
         button.innerHTML = "<i class='fas fa-flask'></i>Import DA"
         button.addEventListener("click", presentDialog)
 
-        $("#sidebar>.scenes-sidebar>.directory-header>.action-buttons").append(button)
+        $('#sidebar>#sidebar-content>#scenes .directory-footer.action-buttons').append(button)
     })
 
 
